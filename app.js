fe663ca78a01f85c60d101fbac40291f20b040c8
LautaDurante <durantelautarop@gmail.com>
2022-10-11 10:56:52 -0300

    agregado de rutas

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+require('dotenv').config();
 
 //Importacion para la conexion a la BD
 const dbConnect = require('./db/connectionDB');
+dbConnect()
+
+//Se inicializa express
+const app = epress();
+//sus configuraciones
+const port = process.env.PORT;
+
+//Middlewares
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
+
+//importacion de Rutas
+
+//Inicio del Servidor
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`));
