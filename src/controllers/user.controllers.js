39f85986c89667d46c3368447b1849fba3d6caf8
LautaDurante <durantelautarop@gmail.com>
2022-10-12 12:51:15 -0300

    parece que anda

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,145 @@
+// Importación de las dependencias
+const Users = require('../models/Users');
+const bcrypt = require('bcrypt');
+
+//Inicializado del Controlador.object
+const CtrlUser = {}
+
+//Controlador de GetUsers
+CtrlUser.getUsers = async (req,res) => {
+    try {
+        const Users = await Users.find({isActive: true});
+        if(Users.length){return res.status(404).json({message:"No se encontró ningún usuario."})}
+
+        return res.json({
+            message: `Número de Usuarios encontrados ${Users.length}`,
+            users: Users
+        })
+    }   catch(error) {
+        return res.status(500).json({message:`No se pudieron encontrar los usuarios: ${error.message}`})
+    }
+}
+
+//Controlador de GetUser por ID
+CtrlUser.getUserID = async (req, res) => {
+    try {
+        const idUser = req.params.idUser;
+        const user = await Users.findOne({$and:[{"_id":idUser},{isActive:true}]});
+        if(!user){
+            return res.status(404).json(
+                {
+                    message:"No se encuentra el usuario"
+                }
+            )
+        }
+        if(!( (idUser == req.user._id) || req.user.role === 'user_admin') ){
+            return res.status(401).json(
+                {
+                    message: `No está autorizado para esta petición.`
+                }
+            )
+        }
+        return res.json(
+            {
+                message: `Usuario encontrado`,
+                user
+            }
+        )
+    } catch (error) {
+        return res.status(500).json({message: `Error interno del Servidor: ${error.message}`})
+    }
+}
+//Controlador de PostUser
+CtrlUser.postUser = async (req, res) => {
+    try {
+        const {username, password,email} = req.body;
+        if(username.length < 8 && password.length < 8){
+            return res.status(404).json({
+                message:"La extensión mínima del nombre de usuario o contraseña es menor a 8"
+            })
+        }
+        // Encriptando la contraseña de usuario
+        const newPassword = bcrypt.hashSync(password,10);//Mecanismo de encriptado de la contraseña
+
+        const newUser = new Users({
+            username,
+            password: newPassword,
+            email
+        });
+        await newUser.save();
+        return res.status(201).json({
+            message: `Usuario guardado correctamente.`
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: `Hubo un error con guardar el usuario: ${error.message}`
+        })
+    }
+}
+//Controlador de PutUser
+CtrlUser.putUser = async (req, res) => {
+    try {
+        const idUser = req.params.idUser;
+        const {password,email} = req.body;
+        if(!idUser || !password || !email) {
+            return res.status(400).json({
+                message: `No viene id o información en la petición.`,
+                opcionesDeCuerpo:["password","email"]
+            })
+        }
+        if(password.length < 8){
+            return res.status(404).json({
+                message: `La extensión de la contraseña debe ser mayor a 8`
+            })
+        }
+        const User = await USER.findOne({$and:[{_id: idUser}, {isActive: true}]});
+        if(!User){
+            return res.status(404).json({
+                message: `El usuario no fue encontrado`
+            })
+        }
+        if(!( (idUser == req.user._id) || req.user.role === 'user_admin') ){
+            return res.status(401).json(
+                {
+                    message: `No está autorizado para esta petición.`
+                }
+            )
+        }
+        const newPassword = bcrypt.hashSync(password,10)
+        await User.updateOne({password:newPassword , email});
+        return res.status(201).json({
+            message: `Usuario modificado correctamente.`
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: `Hubo un error con modificar el usuario: ${error.message}`
+        })
+    }
+}
+//Controlador de DeleteUser
+CtrlUser.deleteUser = async (req, res) => {
+    try {
+        const idUser = req.params.idUser;
+        const user = await Users.findOne({$and:[{_id: idUser},{isActive: true}]});
+        if(!user){
+            return res.status(404).json({
+                message: `El usuario ya no existe`
+            })
+        }
+        if(!( (idUser == req.user._id) || req.user.role === 'user_admin') ){
+            return res.status(401).json(
+                {
+                    message: `No está autorizado para esta petición.`
+                }
+            )
+        }
+        await user.updateOne({isActive: false})
+        return res.status(201).json({
+            message: `Usuario eliminado correctamente.`
+        })
+    } catch (error) {
+        return res.status(500).json({message:`Error interno del servidor: ${error.message}`})
+    }
+}
+//Export del Controlador
+module.exports = CtrlUser;
---
+++ src/middlewares/es-admin.js
@@ -0,0 +1,12 @@
+//Creado de la fn que valida si el rol de usuario es admin
+const isAdmin = (req, res, next) => {
+    if(req.user.role !== 'user_admin'){
+        return res.status(401).json({
+            message: 'No autorizado - No eres administrador'
+        })
+    }
+
+    next();
+}
+
+module.exports = isAdmin;
